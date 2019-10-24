42f856f46d519dcc7fa42fcdb60eaddd043eb8b2
James Botwina <34082590+Jimbo16@users.noreply.github.com>
2019-10-23 20:54:09 -0400

    Create code.py

---
+++ Pomodorro/code.py
@@ -0,0 +1,34 @@
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import neopixel
+
+pixels = neopixel.NeoPixel(board.A0, 5, brightness=.02, auto_write=True)
+
+button_c = DigitalInOut(board.A2) #pin 0 on the board
+button_c.direction = Direction.INPUT
+button_c.pull = Pull.UP
+
+def blinkRest():
+    count = 0
+    while (count < 5):
+        for i in range(0,5):
+            pixels[i] = (0, 100, 0)
+            time.sleep(.5)
+        for i in range(0,5):
+            pixels[i] = (0, 0, 0)
+            time.sleep(.5)
+        count+=1
+
+while True:
+    buttonClicked = not button_c.value
+    if buttonClicked:
+        for i in range(0,5):
+            pixels[i] = (100, 0, 0)
+            time.sleep(5)
+        
+        blinkRest()
+        
+        for i in range(0,5):
+            pixels[i] = (0,0,0)
+    time.sleep(.2)
